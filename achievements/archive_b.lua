c9ed343b5d265dbfbac3c8f5d70b7644655d07e0
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2023-08-30 03:49:41 +0200

    Fixed armor achievement

--- achievements/archive_b.lua
+++ achievements/archive_b.lua
@@ -4,37 +4,32 @@ local module = {}
 -- Text : Have Mech Armor absorb 5 damage in a single battle
 -- Code : Have a mech with armored be attacked or be there at turn start with a Psion Tyrant alive
 
-local function register_armor(skillEffect, effects)
+local function register_armor(skillEffect, effects, f)
     if effects == nil then
-        return {}
+        return
     end
 
-    local new_effects = {}
     for i = 1, effects:size() do
         local space_damage = effects:index(i)
         local damage = space_damage.iDamage
-        if damage ~= 0 and damage ~= DAMAGE_ZERO and damage ~= DAMAGE_DEATH then
-            local loc = space_damage.loc
-            local pawn = Board:GetPawn(loc)
-            if pawn ~= nil and pawn:IsPlayer() and pawn:IsArmor() then
-                new_effects[i] = "mod_loader.mods[\"randomizer\"].check_attacked_armor(" .. pawn:GetId() .. ")"
-            end
+
+        if damage > 0 and damage < DAMAGE_ZERO then
+            mod_loader.mods["randomizer"].archive_b_1 = module.achievement1
+            local previous_space_damage = effects:index(i)
+            previous_space_damage.sScript = 
+            "local pawn = Board:GetPawn(Point(" .. space_damage.loc.x .. ", " .. space_damage.loc.y .. "))\n"
+            .. "if pawn ~= nil and pawn:IsPlayer() and pawn:IsArmor() and not pawn:IsAcid() and not pawn:IsShield() then\n"
+            .. "mod_loader.mods[\"randomizer\"].archive_b_1:addProgress(1)\n"
+            .. "end\n"  --The pawn sometimes isn't already there, so I check what's there just after the damage
+            .. (previous_space_damage.sScript or "")
         end
     end
-    return new_effects
 end
 
 local function register_attack(mission, pawn, weaponId, p1, p2, skillEffect)
     if module.achievement1:is_active() then
-        local scripts = register_armor(skillEffect, skillEffect.effect)
-        for _, script in pairs(scripts) do
-            skillEffect:AddScript(script)
-        end
-
-        scripts = register_armor(skillEffect, skillEffect.q_effect)
-        for _, script in pairs(scripts) do
-            skillEffect:AddQueuedScript(script)
-        end
+        register_armor(skillEffect, skillEffect.effect, skillEffect.AddScript)
+        register_armor(skillEffect, skillEffect.q_effect, skillEffect.AddQueuedScript)
     end
 end
 
@@ -76,14 +71,6 @@ function module.initialize_achievement_1(achievement, mod)
     modapiext.events.onSkillBuild:subscribe(register_attack)
     randomizer_helper.events.on_vek_action_change:subscribe(handle_tentacles)
     modApi.events.onMissionStart:subscribe(reset_armor)
-
-    local randomizer_mod = mod_loader.mods["randomizer"]
-    function randomizer_mod.check_attacked_armor(mech_id)
-        local mech = Board:GetPawn(mech_id)
-        if not mech:IsAcid() and not mech:IsShield() then
-            module.achievement1:addProgress(1)
-        end
-    end
 end
 
 -- ACHIEVEMENT 2
