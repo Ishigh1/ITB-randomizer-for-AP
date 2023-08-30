b2ee3a85827ba2c352c4bc05c805d3b17400e7b3
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2023-08-30 03:03:21 +0200

    Fixing detritus A achievements

--- achievements/detritus_a.lua
+++ achievements/detritus_a.lua
@@ -4,27 +4,31 @@ local module = {}
 -- Text : Have the Chain Whip attack chain through 10 tiles
 -- Code : Have any weapon affect 10 tiles
 
-local function register_attack(mission, pawn, weaponId, p1, p2, skillEffect)
-    if module.achievement2:is_active() then
-        local effects = skillEffect.effect
-        if effects == nil then
-            return
-        end
+local function handle_effect(effects, skillEffect, method)
+    if effects == nil then
+        return
+    end
 
-        local affected = 0
-        local locs = {}
-        for i = 1, effects:size() do
-            local space_damage = effects:index(i)
-            local loc = space_damage.loc
-            if locs[loc] == nil then
-                affected = affected + 1
-                locs[loc] = 1
-                if affected == 10 then
-                    mod_loader.mods["randomizer"].pinnacle_a_1 = module.achievement1
-                    skillEffect:AddScript("mod_loader.mods[\"randomizer\"].pinnacle_a_1:addProgression(true)")
-                end
+    local affected = 0
+    local locs = {}
+    for i = 1, effects:size() do
+        local space_damage = effects:index(i)
+        local loc = space_damage.loc
+        if locs[loc] == nil then
+            affected = affected + 1
+            locs[loc] = 1
+            if affected == 10 then
+                mod_loader.mods["randomizer"].pinnacle_a_1 = module.achievement1
+                skillEffect[method](skillEffect, "mod_loader.mods[\"randomizer\"].pinnacle_a_1:addProgress(true)")
             end
         end
+    end
+end
+
+local function register_attack(mission, pawn, weaponId, p1, p2, skillEffect)
+    if module.achievement1:is_active() then
+        handle_effect(skillEffect.effect, skillEffect, "AddScript")
+        handle_effect(skillEffect.q_effect, skillEffect, "AddQueuedScript")
     end
 end
 
@@ -40,14 +44,14 @@ end
 
 local function reset_speedrun()
     if module.achievement2:is_active() then
-        module.achievement2:set_flag("start", os.time())
-        module.achievement2:resetProgression()
+        module.achievement2:set_data("start", os.time())
+        module.achievement2:resetProgress()
     end
 end
 
 local function check_speedrun(island)
-    if module.achievement2:is_active() and (os.time() - module.achievement2:get_flag("start") / 60 <= 30)  then
-        module.achievement2:addProgression(1)
+    if module.achievement2:is_active() and (os.time() - module.achievement2:get_data("start") / 60 <= 30)  then
+        module.achievement2:addProgress(1)
     end
 end
 
@@ -67,16 +71,16 @@ local function notice_block()
     end
 end
 
-local function reset_block()
-    if module.achievement3:is_active() then
+local function reset_block(action)
+    if action == ATTACK_ORDER_IDLE and module.achievement3:is_active() then
         module.achievement3:resetProgress()
     end
 end
 
 function module.initialize_achievement_3(achievement, mod)
     achievement.objective = 4
-    GameEvents.onSpawnBlocked:subscribe(notice_block)
-    GameEvents.onEnemyTurn:subscribe(reset_block)
+    GameEvents["onSpawnBlocked"]:subscribe(notice_block)
+    randomizer_helper.events.on_vek_action_change:subscribe(reset_block)
 end
 
 return module
