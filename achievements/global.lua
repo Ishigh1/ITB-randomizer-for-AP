36b130a7b832f7452e33e54d7087c3a851712750
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2023-10-21 20:24:57 +0200

    Added bombers

--- achievements/global.lua
+++ achievements/global.lua
@@ -20,37 +20,45 @@ end
 
 function module.add_achievements()
     module.name_to_id = {}
-    local squad_names = {
-        "Archive_A",
+    local squad_names = 
+    {
+        "Archive_A", 
         "Rust_A", 
         "Pinnacle_A", 
-        "Detritus_A",
+        "Detritus_A", 
         "Archive_B", 
-        "Rust_B",
+        "Rust_B", 
         "Pinnacle_B",
-        "Detritus_B"
+        "Detritus_B", 
+        "", -- secret doesn't have any achievement
+        "Squad_Bomber"
     }
 
+    local image_prefix = "img/achievements/"
     for squad_index, squad_name in ipairs(squad_names) do
-        for i = 1, 3, 1 do
-            local name = GetVanillaText("Ach_" .. squad_name .. "_" .. i .. "_Title")
-            local tooltip = GetVanillaText("Ach_" .. squad_name .. "_" .. i .. "_Text")
-            local progress_key = "Ach_" .. squad_name .. "_" .. i .. "_Progress"
-            local progress = GetVanillaText(progress_key)
-            if progress ~= progress_key then
-                progress = string.gsub(progress, "%$1", "$")
-                tooltip = tooltip .. "\n" .. progress
+        if squad_name == "" then
+            image_prefix = "img/advanced/achievements/"
+        else
+            for i = 1, 3, 1 do
+                local name = GetVanillaText("Ach_" .. squad_name .. "_" .. i .. "_Title")
+                local tooltip = GetVanillaText("Ach_" .. squad_name .. "_" .. i .. "_Text")
+                local progress_key = "Ach_" .. squad_name .. "_" .. i .. "_Progress"
+                local progress = GetVanillaText(progress_key)
+                if progress ~= progress_key then
+                    progress = string.gsub(progress, "%$1", "$")
+                    tooltip = tooltip .. "\n" .. progress
+                end
+
+                local achievement = {
+                    id = name,
+                    name = name,
+                    tooltip = tooltip,
+                    image = image_prefix .. squad_name .. "_" .. i .. ".png",
+                    squad = "squad" .. squad_index
+                }
+
+                initialize_achievement(achievement, squad_name, i, name)
             end
-
-            local achievement = {
-                id = name,
-                name = name,
-                tooltip = tooltip,
-                image = "img/achievements/" .. squad_name .. "_" .. i .. ".png",
-                squad = "squad" .. squad_index
-            }
-
-            initialize_achievement(achievement, squad_name, i, name)
         end
     end
 end
