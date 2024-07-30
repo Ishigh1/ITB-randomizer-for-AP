d74682a427b6cc25f4b77d482a20133e5e15c7eb
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2024-07-30 22:29:25 +0200

    Incremented version number

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ return {
     id = "randomizer",
     name = "Randomizer",
     description = "AP randomizer",
-    version = "0.14.3",
+    version = "0.15.1",
     modApiVersion = "2.9.3",
     requirements = {},
     metadata = metadata,
